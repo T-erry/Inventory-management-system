8873f982d1288a9224697460557028f0e0fa1f0b
T-erry <terrywachuka10@gmail.com>
2024-11-11 01:22:05 +0300

    created an inventory model

--- inventorySystem/inventory/models.py
+++ inventorySystem/inventory/models.py
@@ -2,7 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Inventory(models.Model):
-    name = models.charField(max_length=100, null=False, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
     cost_per_item = models.DecimalField(max_digits=19, decimal_places=2, null=False, blank=False)
-    quantity_in_stock = models.IntegerField(null=False)
+    quantity_in_stock = models.IntegerField(null=False, blank=False)
+    quantity_sold = models.IntegerField(null=False, blank=False)
+    sales = models.DecimalField(max_digits=19, decimal_places=2, null=False, blank=False)
+    stock_date = models.DateField(auto_now_add=True)
+    last_sale_date = models.DateField(auto_now=True)
+
+
+def __str__(self):
+    return self.name
+
+
 
