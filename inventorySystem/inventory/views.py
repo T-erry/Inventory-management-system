daf63e49e2747d237b9924f328e0b2b30f071d6d
T-erry <terrywachuka10@gmail.com>
2024-11-13 15:21:48 +0300

    created homepage view

--- inventorySystem/inventory/views.py
+++ inventorySystem/inventory/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context ={
+        "title" : "Index Page"
+    }
+    return render(request, "inventory/index.html", context=context)
