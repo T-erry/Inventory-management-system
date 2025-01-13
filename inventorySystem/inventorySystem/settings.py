e219feaf6f7d121d50b25ff8b47cd36c215c255b
T-erry <terrywachuka10@gmail.com>
2025-01-13 14:21:12 +0300

    refactored the setting.py file

--- inventorySystem/inventorySystem/settings.py
+++ inventorySystem/inventorySystem/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
+load_dotenv()  # take environment variables from .env.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,8 +143,8 @@ LOGIN_URL = "/login"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'wanjasam2@gmail.com'
-EMAIL_HOST_PASSWORD = 'mhub bljr qevu yjkc'
+EMAIL_HOST_USER = os.environ.get('EMAILL_ADDRESS')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAILL_HOST_PASSWORD')
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 
